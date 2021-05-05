2adfe29ada9f177297e2422b43d0b602ff9c91c8
Ubuntu <info.freedox@gmail.com>
2021-05-05 10:18:25 +0000

    v2

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_080856) do
+ActiveRecord::Schema.define(version: 2021_05_05_092017) do
 
-  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
